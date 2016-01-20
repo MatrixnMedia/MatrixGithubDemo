6215e53361157e82cb52ac27bd9aebf790fffd88
parna <parna@matrixnmedia.com>
2016-01-20 12:39:26 +0530

    intent added and layout file changed

--- app/src/main/java/app/matrixgithubdemo/MainActivity.java
+++ app/src/main/java/app/matrixgithubdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package app.matrixgithubdemo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,14 +11,24 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        context = this;
+
+        btn=(Button)findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+
         setSupportActionBar(toolbar);
         Log.e("Hello", "Sutapa");
 
@@ -24,9 +36,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
-                Log.d("d", "@nd change parnaaaa!!!");
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+               /* Log.d("d", "@nd change parnaaaa!!!");*/
 
             }
         });
@@ -52,5 +64,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn:
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
